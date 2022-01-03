be9df6258e168144c126b9d2e40f0e6de22d2c23
AdvaitaRaghavan <97016219+AdvaitaRaghavan@users.noreply.github.com>
2022-01-03 21:18:32 +0800

    Update Cypher.ino

--- Cypher.ino
+++ Cypher.ino
@@ -3,6 +3,9 @@
 void setup(void) {
 char *token;
 char *mystring = "A,B,C";
+string1 =""
+string2 =""
+string3 =""
 const char *delimiter =",";
 
    Serial.begin(115200);
@@ -19,7 +22,12 @@ const char *delimiter =",";
     Serial.println(token);
     token=strtok(NULL, delimiter);
    }
-
+   string1 = "F";
+   string2= "G"
+   string3= "H"
+   string2+= string3
+   string1+= string2
+   Serial.println(string1)
 }
 
 void loop(void) {
